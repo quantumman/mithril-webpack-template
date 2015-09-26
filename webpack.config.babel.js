45c15b1ccb1c7aa1a190e8eefd2e1985ff53dc2f
Shodai Yokoyama <quantumcars@gmail.com>
2015-09-27 03:02:11 +0900

    Add webpack.config.babel.js

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import webpack from 'webpack';
+import path from 'path';
+
+module.exports = {
+  entry: {
+    main: './client/app.js'
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    preLoaders: [
+      { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'eslint-loader' }
+    ],
+    loaders: [
+      { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel' },
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.js', '.jsx', '.css']
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      m: 'mithril'
+    })
+  ],
+  eslint: {
+    configFile: '.eslintrc'
+  }
+};
